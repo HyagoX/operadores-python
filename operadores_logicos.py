627e6ada29c617fc0b27300a5277bfa50e4b2dc0
Hyago <159494212+HyagoX@users.noreply.github.com>
2025-08-21 22:54:42 -0300

    Add files via upload

---
+++ operadores_logicos.py
@@ -0,0 +1,18 @@
+print(True and True)
+print(True and False)
+print(False and False)
+print(True or True)
+print(True or False)
+print(False or False)
+
+saldo = 1000
+saque = 250
+limite = 200
+conta_especial = True
+
+print(True and True)
+
+# And(E)
+saldo >= saque and saque <= limite
+
+saldo >= saque or saque <= limite
